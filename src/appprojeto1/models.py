69c0046bcd4c34fd799af0c9af546e0a5b324457
cettdev3 <desenvolvedor3@cett.org.br>
2023-04-12 10:43:04 -0300

    alteracao na model edital

--- src/appprojeto1/models.py
+++ src/appprojeto1/models.py
@@ -240,7 +240,7 @@ class Edital(models.Model):
     status = models.CharField(max_length=255)
     pdf = models.CharField(max_length=255, null=True, blank=True)
     motivo = models.CharField(max_length=255, blank=False, null=True)
-
+    user_change = models.ForeignKey(User,on_delete = models.DO_NOTHING )
     def __str__(self):
         return self.num_edital
 
