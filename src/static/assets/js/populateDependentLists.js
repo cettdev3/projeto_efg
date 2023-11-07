c9a854968c51b4fc1ae1186c74a0fb38bb7b6cf0
Gustavo Côrtes <desenvolvedor1@cett.org.br>
2023-11-07 12:09:54 -0300

    Carregar eixo e escola ao atualizar a tela

--- src/static/assets/js/populateDependentLists.js
+++ src/static/assets/js/populateDependentLists.js
@@ -1,21 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const id_escola = document.getElementById('id_escola');
+    populateDependentLists(id_escola);
+})
+
 function populateDependentLists(source) {
     const id_escola = $('#id_escola').val();
     const id_tipo_curso = $('#id_tipo_curso').val();
     const id_modalidade = $('#id_modalidade').val();
     const id_eixo = $('#id_eixo').val();
+    const id_curso = $('#id_curso').val();
 
     if (source.id === 'id_escola' || source.id === 'id_tipo_curso' || source.id === 'id_modalidade') {
-        console.log('Populando Eixo')
-        popularEixo(id_escola);
-        console.log('Populando curso')
-        popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo);
+        popularEixo(id_escola, id_eixo);
+        popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo, id_curso);
     }
 
     if (source.id === 'id_eixo') {
-        popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo);
+        popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo, id_curso);
     }
 
-    function popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo) {
+    function popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo, id_curso) {
         $.ajax({
             url: '/ajax/get_cursos/',
             type: 'GET',
@@ -31,11 +35,14 @@ function populateDependentLists(source) {
                 $.each(data, function (key, value) {
                     $('#id_curso').append('<option value="' + value.id + '">' + value.curso + '</option>');
                 });
+                if (id_curso !== '') {
+                    $('#id_curso').val(id_curso);
+                }
             }
         });
     };
 
-    function popularEixo(id_escola) {
+    function popularEixo(id_escola, id_eixo) {
         $.ajax({
             url: '/ajax/get_eixos/',
             type: 'GET',
@@ -48,6 +55,9 @@ function populateDependentLists(source) {
                 $.each(data, function (key, value) {
                     $('#id_eixo').append('<option value="' + value.eixo_id + '">' + value.nome + '</option>');
                 });
+                if (id_eixo !== '') {
+                    $('#id_eixo').val(id_eixo);
+                }
             }
         });
     };
--- src/staticfiles/assets/js/populateDependentLists.js
+++ src/staticfiles/assets/js/populateDependentLists.js
@@ -1,21 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const id_escola = document.getElementById('id_escola');
+
+    console.log(id_escola)
+    populateDependentLists(id_escola);
+})
+
 function populateDependentLists(source) {
     const id_escola = $('#id_escola').val();
     const id_tipo_curso = $('#id_tipo_curso').val();
     const id_modalidade = $('#id_modalidade').val();
     const id_eixo = $('#id_eixo').val();
+    const id_curso = $('#id_curso').val();
 
     if (source.id === 'id_escola' || source.id === 'id_tipo_curso' || source.id === 'id_modalidade') {
         console.log('Populando Eixo')
-        popularEixo(id_escola);
+        popularEixo(id_escola, id_eixo);
         console.log('Populando curso')
-        popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo);
+        popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo, id_curso);
     }
 
     if (source.id === 'id_eixo') {
-        popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo);
+        popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo, id_curso);
     }
 
-    function popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo) {
+    function popularCurso(id_escola, id_tipo_curso, id_modalidade, id_eixo, id_curso) {
         $.ajax({
             url: '/ajax/get_cursos/',
             type: 'GET',
@@ -31,11 +39,14 @@ function populateDependentLists(source) {
                 $.each(data, function (key, value) {
                     $('#id_curso').append('<option value="' + value.id + '">' + value.curso + '</option>');
                 });
+                if (id_curso !== '') {
+                    $('#id_curso').val(id_curso);
+                }
             }
         });
     };
 
-    function popularEixo(id_escola) {
+    function popularEixo(id_escola, id_eixo) {
         $.ajax({
             url: '/ajax/get_eixos/',
             type: 'GET',
@@ -48,6 +59,9 @@ function populateDependentLists(source) {
                 $.each(data, function (key, value) {
                     $('#id_eixo').append('<option value="' + value.eixo_id + '">' + value.nome + '</option>');
                 });
+                if (id_eixo !== '') {
+                    $('#id_eixo').val(id_eixo);
+                }
             }
         });
     };
